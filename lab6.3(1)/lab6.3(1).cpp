524d8b555db77800a9d145fec38326ba109c364a
Victiria <Victiria@DESKTOP-R2P5S19>
2021-11-28 15:31:54 +0200

    Добавьте файлы проекта.

---
+++ lab6.3(1)/lab6.3(1).cpp
@@ -0,0 +1,23 @@
+﻿// lab6.3(1).cpp 
+// Опрацювання одновимірних масивів за допомогою звичайних функцій та шаблонів
+// Сацик Вікторія
+//17 варіант
+#include <iostream>
+using namespace std;
+void Input(int* a, const int size) {
+	for
+		(int i = 0; i < size; i++)
+	{
+		cout << "a[" << i << "] = "
+			;
+
+		cin >> a[i];
+	}
+	cout << endl;
+}
+int main() {
+	const int n = 5;
+	int a[n];
+	Input(a, n);
+	return 0;
+}
